5e3cf864dc4fda308efedd1796e4b49f75dffb24
armaankhan786 <110148280+armk786@users.noreply.github.com>
2024-04-29 17:03:07 +0530

    Fixed styling

--- application/views/admin/view_login.php
+++ application/views/admin/view_login.php
@@ -12,7 +12,7 @@
         <link rel="shortcut icon" href="<?=base_url()?>assets/images/fev.png">
 
         <!-- Bootstrap Css -->
-        <link href="<?=base_url()?>assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <!-- Icons Css -->
         <link href="<?=base_url()?>assets/css/icons.min.css" rel="stylesheet" type="text/css" />
         <!-- App Css-->
